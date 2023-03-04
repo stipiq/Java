d4e23d9f7137d892ae50397568971e78643b5f0e
Ivan Stipic <ivanstipic2020@gmail.com>
2023-03-04 19:37:08 +0100

    Commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double a,b;
+        String operation;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type the first number: ");
+        a = scanner.nextDouble();
+        System.out.print("Type the second number: ");
+        b = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Type the operation: ");
+        operation = scanner.nextLine().trim();
+        double result;
+        if (operation.equals("+"))
+            result = a + b;
+        else if (operation.equals("-"))
+            result = a - b;
+        else if (operation.equals("/"))
+            result = a / b;
+        else
+            result = a * b;
+        System.out.println(result);
+        //code working
+    }
+}
